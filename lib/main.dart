4fbd8c872972bc1dbfd53451e4f0e36f2908622f
rafael.marinho <rafael.marinho@luizalabs.com>
2021-05-25 18:08:20 -0300

    feat: create to do list screen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Import Screen
+import 'package:todo_list/screens/list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListScreen(),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          scaffoldBackgroundColor: Colors.grey[900]),
+    );
+  }
+}
---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Personal List',
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Divider(),
+            SizedBox(
+              height: 20.0,
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'My personal Item',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey[200]),
+                    ),
+                    leading: Container(
+                      width: 25.0,
+                      height: 25.0,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          shape: BoxShape.circle),
+                    ),
+                  );
+                })
+          ],
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {},
+      ),
+    );
+  }
+}
