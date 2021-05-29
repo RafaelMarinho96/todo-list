20c664e3cdc855be29ecf953b61820846c8a6772
rafael.marinho <rafael.marinho@luizalabs.com>
2021-05-29 16:53:54 -0300

    feat: implement splash screen with lottie animation

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+// Lottie Import
+import 'package:lottie/lottie.dart';
+import 'package:todo_list/screens/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MainScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: Lottie.asset('assets/splash_animation.json'),
+            ),
+            Text('Check List',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 20),
+            Text(
+              'The easiest way to create and manage yours daily tasks routines',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 300,
+            width: 300,
+            child: Lottie.asset('assets/splash_animation.json'),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text('Easy List')
+        ],
+      ), */
